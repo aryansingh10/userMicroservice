46ec6ae36a0bd8536e38870cf829aec2f91d0e1d
Aryan Singh Thakur <aryansingh102003@gmail.com>
2025-02-09 21:16:10 +0530

    Create user Mutation Added

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,16 +1,35 @@
-import { Resolver, Query, ResolveReference } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Args,
+  ResolveReference,
+  Mutation,
+} from '@nestjs/graphql';
 import { User } from './user.model';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Resolver(() => User)
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
 
+  // Query to fetch all users
   @Query(() => [User], { name: 'users' })
   async getUsers() {
     return this.userService.findAll();
   }
 
+  // Query to fetch user by ID
+  @Query(() => User, { name: 'user', nullable: true })
+  async getUserById(@Args('id') id: number) {
+    return this.userService.findById(id);
+  }
+
+  @Mutation(() => String)
+  async createUser(@Args('input') input: CreateUserDto) {
+    return this.userService.createUser(input);
+  }
+  // ResolveReference for Apollo Federation
   @ResolveReference()
   async resolveReference(reference: { __typename: string; id: number }) {
     return this.userService.findById(reference.id);
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { users } from '../database/schema';
 import { DrizzleClient } from './types';
-import { sql } from 'drizzle-orm';
+import { eq, sql } from 'drizzle-orm';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
@@ -20,5 +21,15 @@ export class UserService {
       .from(users)
       .where(sql`${users.id} = ${id}`);
     return user;
+  }
+
+  async createUser(input: CreateUserDto): Promise<string> {
+    const result = await this.db.insert(users).values(input).execute();
+
+    if (!result || !result[0]?.insertId) {
+      throw new Error('User creation failed');
+    }
+
+    return `User created successfully with ID: ${result[0].insertId}`;
   }
 }
